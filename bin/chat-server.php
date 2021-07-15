48988ada58e790a87b72643151cae36123e068ad
Solomon Bareebe <baberuka@gmail.com>
2021-07-15 19:04:42 +0200

    Update chat-server.php

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -2,17 +2,14 @@
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
-use ratchetApp\Chat;
-
-    require dirname(__DIR__) . '/vendor/autoload.php';
-
-    $server = IoServer::factory(
-        new HttpServer(
-            new WsServer(
+use RatchetApp\Chat;
+require dirname(__DIR__).'/chat.php';
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
                 new Chat()
-            )
-        ),
-        8080
-    );
-
-    $server->run();
+        )
+    ),
+8080
+);  
+$server->run(); 
